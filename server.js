06883324f036332df20ca49f46cca90fd35baa56
Pedro Lana <pedrolana@hotmail.com>
2020-05-19 13:43:23 -0300

    Integrando ao backend

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
+server.use(express.static('public'));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express:server
+})
+
 server.get("/", function(req, res){
-    return res.send("Hi!")
+    return res.render("index");
 })
 
 server.listen(3000, function(){
